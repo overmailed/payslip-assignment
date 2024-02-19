f0a3be377cc4b1b1fc716a36a6cdf3ad981fb544
overmailed <overmailed@gmail.com>
2024-02-19 04:17:59 +0400

    add payslip list item

--- src/components/PayslipListItem/PayslipListItem.tsx
+++ src/components/PayslipListItem/PayslipListItem.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
 import { IonIcon, IonItem, IonLabel } from '@ionic/react';
 import { reader } from 'ionicons/icons';
+import { DateTime } from 'luxon';
 
 import { Payslip } from '../../models/Payslip';
 
@@ -9,10 +11,23 @@ type PayslipListItemProps = {
 }
 
 export const PayslipListItem: FC<PayslipListItemProps> = ({ payslip }) => {
+  const { t } = useTranslation();
+
   return (
-      <IonItem role="article">
-        <IonIcon icon={reader} size="large" color="primary" aria-hidden="true"/>
-        <IonLabel>{payslip.id}</IonLabel>
-      </IonItem>
+    <IonItem role="article" href={`/payslip/${payslip.id}`}>
+      <IonIcon slot='start' icon={reader} size="large" color="primary" aria-hidden="true"/>
+      <IonLabel>
+        <h3>
+          <strong>{t('dateFrom')}</strong> {DateTime.fromJSDate(payslip.fromDate).toLocaleString(
+            DateTime.DATE_FULL,
+          )}
+        </h3>
+        <h3>
+          <strong>{t('dateTo')}</strong> {DateTime.fromJSDate(payslip.toDate).toLocaleString(
+            DateTime.DATE_FULL,
+          )}
+        </h3>
+      </IonLabel>
+    </IonItem>
   )
 }
--- src/services/payslips.ts
+++ src/services/payslips.ts
@@ -5,6 +5,7 @@ import { Payslip } from '../models/Payslip';
 
 const API_DELAY = 300;
 const API_MAX_PAGES = 5;
+const DATE_SHIFT = 30 * 24 * 360 * 10000;
 
 type PayslipPagination = {
   page: number;
@@ -23,8 +24,8 @@ export const getPayslipListPage = async (
 ): Promise<PayslipsReslut> => {
   const fakeItems = new Array<unknown>(pageSize).fill(undefined).map<Payslip>((item, index) => ({
     id: uuidv4(),
-    fromDate: new Date(Date.now() - ( page * pageSize + index) * 86400000),
-    toDate: new Date(Date.now() - (page * pageSize + index + 1) * 86400000),
+    fromDate: new Date(Date.now() - ( page * pageSize + index) * DATE_SHIFT),
+    toDate: new Date(Date.now() - (page * pageSize + index + 1) * DATE_SHIFT),
     file: "somefile.file",
   }));
 
