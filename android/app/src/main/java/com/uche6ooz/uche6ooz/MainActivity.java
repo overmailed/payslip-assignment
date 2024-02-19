803ec69366a5a4579d0ea7e9718ea598caaed159
overmailed <overmailed@gmail.com>
2024-02-19 11:17:12 +0400

    add native platforms

---
+++ android/app/src/main/java/com/uche6ooz/uche6ooz/MainActivity.java
@@ -0,0 +1,5 @@
+package com.uche6ooz.uche6ooz;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.uche6ooz.uche6ooz',
   appName: 'payslip-assignmnet',
   webDir: 'dist',
   server: {
